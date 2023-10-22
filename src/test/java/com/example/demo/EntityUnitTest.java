1fac2b09edc368253d273906bcddbd079ecd01ae
Josep Olivé <josepolive89@gmail.com>
2023-10-22 19:30:26 +0200

    Test Setupç

--- src/test/java/com/example/demo/EntityUnitTest.java
+++ src/test/java/com/example/demo/EntityUnitTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
@@ -37,14 +37,14 @@ class EntityUnitTest {
     private Appointment a2;
     private Appointment a3;
 
-    @Test
-    void this_is_a_test(){
-        // DELETE THIS TEST
-        assertThat(false).isEqualTo(true);
-    }
+    @BeforeEach
+    void setUp() {
+        d1 = new Doctor("John", "Doe", 30, "john.doe@example.com");
+        p1 = new Patient("Jane", "Doe", 25, "jane.doe@example.com");
+        r1 = new Room("Room A");
 
-    /** TODO
-     * Implement tests for each Entity class: Doctor, Patient, Room and Appointment.
-     * Make sure you are as exhaustive as possible. Coverage is checked ;)
-     */
+        a1 = new Appointment(p1, d1, r1, LocalDateTime.of(2023, 4, 24, 9, 30), LocalDateTime.of(2023, 4, 24, 10, 30));
+        a2 = new Appointment(p1, d1, r1, LocalDateTime.of(2023, 4, 24, 10, 30), LocalDateTime.of(2023, 4, 24, 11, 30));
+        a3 = new Appointment(p1, d1, r1, LocalDateTime.of(2023, 4, 24, 11, 30), LocalDateTime.of(2023, 4, 24, 12, 30));
+    }
 }
